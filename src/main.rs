e8b795224021c87df4a0a750f042ca7a3c37a7c1
SAWADA Tadashi <cesare@mayverse.jp>
2017-01-19 23:37:49 +0900

    Implement naively

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate rusoto;
+use rusoto::{DefaultCredentialsProvider, Region};
+use rusoto::ec2::{Ec2Client, DescribeHostsRequest};
+
 fn main() {
-    println!("Hello, world!");
+    let provider = DefaultCredentialsProvider::new().unwrap();
+    let region = Region::ApNortheast1;
+    let client = Ec2Client::new(provider, region);
+    let request = DescribeHostsRequest::default();
+
+    match client.describe_hosts(&request) {
+        Ok(results) => {
+            results.hosts.map(|hosts| for host in hosts {
+                println!("{:?}", host);
+            });
+        }
+        Err(error) => println!("{:?}", error),
+    }
 }
