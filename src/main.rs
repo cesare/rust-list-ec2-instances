111dd5a17ba2d9f32576c509eb16985bec994081
SAWADA Tadashi <cesare@mayverse.jp>
2018-03-18 12:46:20 +0900

    Upgrade rusoto to 0.32

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,39 @@
-extern crate rusoto;
-use rusoto::{ProfileProvider, Region};
-use rusoto::ec2::{Ec2Client, DescribeInstancesRequest, Instance};
-use rusoto::default_tls_client;
+extern crate rusoto_core;
+extern crate rusoto_ec2;
+use rusoto_core::{ProfileProvider, Region};
+use rusoto_core::reactor::RequestDispatcher;
+use rusoto_ec2::{DescribeInstancesRequest, DescribeInstancesResult, Ec2, Ec2Client, Instance,
+                 Reservation};
 
-extern crate rustc_serialize;
 extern crate docopt;
+extern crate rustc_serialize;
 use docopt::Docopt;
+
+extern crate futures;
+use futures::future::Future;
 
 use std::error::Error;
 use std::str::FromStr;
+
+struct InstanceStatus {
+    id: String,
+    ip_address: String,
+}
+
+impl InstanceStatus {
+    fn from_instance(instance: &Instance) -> InstanceStatus {
+        InstanceStatus {
+            id: instance.instance_id.clone().unwrap_or("(unknown)".to_owned()),
+            ip_address: instance.public_ip_address.clone().unwrap_or("".to_owned()),
+        }
+    }
+}
+
+impl std::fmt::Display for InstanceStatus {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{} {}", self.id, self.ip_address)
+    }
+}
 
 const USAGE: &'static str = "
 Usage:
@@ -28,7 +53,9 @@ pub struct Args {
 }
 
 fn parse_args() -> Args {
-    Docopt::new(USAGE).and_then(|d| d.decode()).unwrap_or_else(|e| e.exit())
+    Docopt::new(USAGE)
+        .and_then(|d| d.decode())
+        .unwrap_or_else(|e| e.exit())
 }
 
 fn find_provider(args: &Args) -> Result<ProfileProvider, String> {
@@ -50,11 +77,20 @@ fn find_region(args: &Args) -> Result<Region, String> {
     }
 }
 
-fn show_instance(i: &Instance) {
-    println!("{:?} {:?} {:?}",
-             i.instance_id,
-             i.state,
-             i.public_ip_address);
+fn show_instances(is: &Vec<Instance>) {
+    for i in is {
+        println!("{}", InstanceStatus::from_instance(i));
+    }
+}
+
+fn show_reservations(rs: &Vec<Reservation>) {
+    for r in rs {
+        r.instances.as_ref().map(|is| show_instances(&is));
+    }
+}
+
+fn show_result(result: DescribeInstancesResult) {
+    result.reservations.map(|rs| show_reservations(&rs));
 }
 
 fn main() {
@@ -62,17 +98,12 @@ fn main() {
 
     let provider = find_provider(&args).unwrap();
     let region = find_region(&args).unwrap();
-    let client = Ec2Client::new(default_tls_client().unwrap(), provider, region);
+    let client = Ec2Client::new(RequestDispatcher::default(), provider, region);
     let request = DescribeInstancesRequest::default();
 
-    match client.describe_instances(&request) {
-        Ok(results) => {
-            results.reservations.map(|rs| for r in rs {
-                r.instances.map(|is| for i in is {
-                    show_instance(&i);
-                });
-            });
-        }
-        Err(error) => println!("{:?}", error),
-    }
+    let _ = client
+        .describe_instances(&request)
+        .map(|result| show_result(result))
+        .map_err(|e| eprintln!("Error: {}", e))
+        .wait();
 }
