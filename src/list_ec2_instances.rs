494b280c03a9cdc7f49cab394b008a733d6602b7
SAWADA Tadashi <cesare@mayverse.jp>
2018-08-26 01:51:08 +0900

    Use fmt::Display

--- src/list_ec2_instances.rs
+++ src/list_ec2_instances.rs
@@ -2,25 +2,40 @@ extern crate rusoto_core;
 extern crate rusoto_ec2;
 
 use std::default::Default;
+use std::fmt;
 
 use rusoto_core::Region;
-use rusoto_ec2::{Filter, Ec2, Ec2Client, DescribeInstancesRequest, Instance, Reservation, Tag};
+use rusoto_ec2::{Filter, Ec2, Ec2Client, DescribeInstancesRequest, Instance, Reservation};
 
-fn find_name_in_tags(tags: &Option<Vec<Tag>>) -> Option<String> {
-    tags.as_ref()
-        .and_then(|ts| ts.iter().find(|&tag| tag.key == Some("Name".to_string())))
-        .and_then(|tag| tag.value.as_ref().and_then(|ref v| Some(v.to_string())))
+struct InstanceSummary {
+    instance: Instance,
 }
 
-fn find_public_ip_address(instance: &Instance) -> Option<String> {
-    instance.public_ip_address.as_ref().map(|s| s.to_string())
+impl fmt::Display for InstanceSummary {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let public_ip_address = self.find_public_ip_address().unwrap_or("-".to_string());
+        let name = self.find_name_in_tags().unwrap_or("-".to_string());
+        write!(f, "{}\t{}", name, public_ip_address)
+    }
 }
+
+impl InstanceSummary {
+    fn find_name_in_tags(&self) -> Option<String> {
+        self.instance.tags.as_ref()
+            .and_then(|ts| ts.iter().find(|&tag| tag.key == Some("Name".to_string())))
+            .and_then(|tag| tag.value.as_ref().and_then(|ref v| Some(v.to_string())))
+    }
+
+    fn find_public_ip_address(&self) -> Option<String> {
+        self.instance.public_ip_address.as_ref().map(|s| s.to_string())
+    }
+}
+
 
 fn show_instances(is: &Vec<Instance>) {
     for i in is {
-        let public_ip_address = find_public_ip_address(i).unwrap_or("-".to_string());
-        let name = find_name_in_tags(&i.tags).unwrap_or("-".to_string());
-        println!("{}\t{}", name, public_ip_address);
+        let summary = InstanceSummary { instance: i.to_owned() };
+        println!("{}", summary);
     }
 }
 
